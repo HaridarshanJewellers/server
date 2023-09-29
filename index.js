f831bd22b7daeafecaea460811c3c9f3adfb28ac
HaridarshanJewellers <146411605+HaridarshanJewellers@users.noreply.github.com>
2023-09-29 12:25:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -69,12 +69,3 @@ const server = http.createServer(async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3000;
-
-server.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`);
-});
-
-setInterval(function () {
-  _goldDigger();
-}, 2000);
